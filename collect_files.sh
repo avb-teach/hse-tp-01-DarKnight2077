7f1c5eaa8ffe669738d78647e6b4dda2ded0f427
DarKnight2077 <timer.mixfsy@gmail.com>
2025-04-25 22:52:19 +0300

    joy

---
+++ collect_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+input_directory="$1"
+output_directory="$2"
+
+find $input_directory -type f > files_to_copy.txt
+
+
+for file in $(cat files_to_copy.txt)
+do
+cp $file $output_directory
+done
